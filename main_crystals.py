f48decc97d9993d890cbc43e686534694efcbd81
Niki Amini-Naieni <niki@adainnovations.ai>
2025-01-31 02:03:43 +0000

    Update main_crystals.py

--- main_crystals.py
+++ main_crystals.py
@@ -45,9 +45,8 @@ def on_release(event):
     plt.draw()
 
 """
-exemplars = {
-    
-}
+exemplar_image_names = {"ma2035_023": "ma2035_023_internal_exemplar_image.jpg", "ma2035_024": "ma2035_024_internal_exemplar_image.jpg", "ma2035_039": "ma2035_039_internal_exemplar_image.jpg", "ma2035_040": "ma2035_040_internal_exemplar_image.jpg", "ma2035_069": "ma2035_069_internal_exemplar_image.jpg", "ma2035_072": "ma2035_072_internal_exemplar_image.jpg", "ma2035_169": "ma2035_169_internal_exemplar_image.jpg"}
+exemplar_bbox_file_names = {"ma2035_023": "ma2035_023_internal_exemplars.json", "ma2035_024": "ma2035_024_internal_exemplars.json", "ma2035_039": "ma2035_039_internal_exemplars.json", "ma2035_040": "ma2035_040_internal_exemplars.json", "ma2035_069": "ma2035_069_internal_exemplars.json", "ma2035_072": "ma2035_072_internal_exemplars.json", "ma2035_169": "ma2035_169_internal_exemplars.json"}
 def list_files_recursive(path='.'):
     anno_json = {}
     for entry in os.listdir(path):
@@ -83,27 +82,29 @@ def demo(args):
     print(img_names)
 
     # Make predictions.
-    abs_errs = []
+    pred_boxes = {}
 
     for img_f_name in img_names:
         image = Image.open(img_f_name)
         width, height = image.size
         # Get exemplars.
-        if "ma
-        
-        bounding_boxes = []
-        for i in range(2):
-          if i == 0:
-              idx = random.randint(0, int(data['boxes'].shape[1] / 2))
-          else:
-              idx = random.randint(int(data['boxes'].shape[1] / 2) - 1, data['boxes'].shape[1] - 1)
+        for sequence in exemplar_image_names:
+            if sequence in img_f_name:
+                exemplar_image_name = exemplar_image_names[sequence]
+                exemplar_bbox_file_name = exemplar_bbox_file_names[sequence]
+                print("Using image: " + str(img_f_name))
+                print("Using exemplar image: " + str(exemplar_image_name))
+                print("Using exemplars: " + str(exemplar_bbox_file_name))
+                break
+        exemplar_image = Image.open(exemplar_image_name)
+        with open(exemplar_bbox_file_name) as exemplar_file:
+            exemplar_data = json.load(exemplar_file)
+        exemplars = []
+        for exemplar in exemplar_data:
+            x0, y0, x1, y1 = exemplar[0], exemplar[1], exemplar[3], exemplar[4]
+            exemplars.append((x0, y0, x1, y1))
 
-          box = data['boxes'][0][i]
-          box2 = [int(k) for k in box]
-          x1, y1, x2, y2 = box2[0], box2[1], box2[0] + box2[2], box2[1] + box2[3]
-          bounding_boxes.append((x1, y1, x2, y2))
-
-        bboxes = torch.tensor(bounding_boxes)
+        bboxes = torch.tensor(exemplars)
 
         img, bboxes, scale = resize(image, bboxes)
         print("img shape: " + str(img.shape))
@@ -116,37 +117,6 @@ def demo(args):
             box = pred_boxes[i]
             xmin, ymin, xmax, ymax = box[0], box[1], box[2], box[3]
             x0, y0, box_w, box_h = xmin.item(), ymin.item(), (xmax - xmin).item(), (ymax - ymin).item()
-            score = predicted_bboxes.fields['scores'][i]
-            pred_annotations['annotations'].append(
-                {
-                    "image_id": img_id,
-                    "category_id": 1,
-                    "bbox": [x0, y0, box_w, box_h],
-                    "score": score.item(),
-                    "id": pred_anno_id,
-                    "area": (box_w * box_h)
-                }
-            )
-            pred_anno_id +=1
-        img_id +=1
-            
-        pred_count = round(denisty_map.sum().item(), 1)
-
-        print("Pred. Count: " + str(pred_count) + ", GT Count: " + str(gt_count))
-        err = abs(pred_count - gt_count)
-        abs_errs.append(err)
-
-    abs_errs = np.array(abs_errs)
-    print("Number of Images Tested: " + str(len(abs_errs)))
-    print("MAE: " + str(np.mean(abs_errs)))
-    print("RMSE: " + str(np.sqrt(np.mean(abs_errs**2))))
-
-    with open("dave_box_preds_carpk.json", 'w') as out_file:
-        json.dump(pred_annotations, out_file)
-
-    with open("carpk_coco.json", 'w') as out_file:
-        json.dump(gt_annotations, out_file)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('DAVE', parents=[get_argparser()])
